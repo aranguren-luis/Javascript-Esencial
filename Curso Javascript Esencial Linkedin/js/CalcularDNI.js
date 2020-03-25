9e528be159575421b8638e53675dd26e5b5d3599
Cristhian Lammoglia <cristhianlam93@gmail.com>
2020-03-25 21:48:23 +0100

    "Act"

--- Curso Javascript Esencial Linkedin/js/CalcularDNI.js
+++ Curso Javascript Esencial Linkedin/js/CalcularDNI.js
@@ -6,8 +6,35 @@ var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y',
   'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 
 function CalcularLetraDNI(){
-    var numero     = document.getElementById('input1').value; 
-    var resultado  = numero%23;
-    const letradni = letras[resultado];
-    document.getElementById('resultado').innerHTML = numero+"-"+letradni;
+  try{
+      var numero     = document.getElementById('input1').value; 
+      var resultado  = numero%23;
+      const letradni = letras[resultado];
+      if (numero.length<8){
+        alert("Error");
+      }
+      else{
+        document.getElementById('resultado').innerHTML = numero+"-"+letradni;
+      }
+       
+    }
+
+  catch{
+    alert("¡Ups! Error en la función Calcular DNI");
+  }   
+};
+
+function CalcularLetraNIE(){
+  try{
+      var numero1    = document.getElementById('input1').value;
+      var resultado1 = numero1%12;
+      let letranie   = resultado1 <= 15 ? "Y" : "B";
+      if(numero1.length<7){alert("Error")}
+      else{
+        document.getElementById("resultado1").innerHTML = numero1+"-"+letranie;
+      }
+    }
+  catch{
+    alert("¡Ups! Error en la función Calcular NIE")
+  };
 };
