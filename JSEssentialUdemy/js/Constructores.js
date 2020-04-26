023fa2879e302b9040bbabf90e88cce7a8bf4eae
crislam93 <39211349+crislam93@users.noreply.github.com>
2020-04-26 19:30:53 +0200

    Constructores

---
+++ JSEssentialUdemy/js/Constructores.js
@@ -0,0 +1,15 @@
+function Apple(x, y, color, score)
+            {
+                this.color = color;
+                this.x     = x;
+                this.y     = y;
+                this.score = score;
+            };
+
+var apple1 = new Apple( 10, 20, "Rojo", 100 );
+var apple2 = new Apple( 10, 20, "Verde", 150 );
+var apple3 = new Apple( 10, 20, "Amarilla", 85.7 );
+
+
+
+
